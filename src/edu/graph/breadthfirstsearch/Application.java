9de125e8abc675d83a279196bf5f05a5ce1161d5
Prasad Gamini <>
2019-02-04 09:18:59 -0600

    BFS

---
+++ src/edu/graph/breadthfirstsearch/Application.java
@@ -0,0 +1,24 @@
+package edu.graph.breadthfirstsearch;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Vertex rootVertex = initGraph();
+        new BreadthFirstSearch().search(rootVertex);
+    }
+
+    private static Vertex initGraph() {
+        Vertex vertex1 = new Vertex(1);
+        Vertex vertex2 = new Vertex(2);
+        Vertex vertex3 = new Vertex(3);
+        Vertex vertex4 = new Vertex(4);
+        Vertex vertex5 = new Vertex(5);
+
+
+        vertex1.addNeighborVertices(vertex2, vertex4);
+        vertex2.addNeighborVertices(vertex1, vertex3);
+        vertex4.addNeighborVertices(vertex5);
+
+        return vertex1;
+    }
+}
