605b3cb23de89b031d66d150b34232391bcdb80b
Prasad Gamini <>
2019-02-07 18:27:18 -0600

    Adding new Project

---
+++ graphs/src/test/java/edu/graph/graphs/GraphsApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.graph.graphs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
