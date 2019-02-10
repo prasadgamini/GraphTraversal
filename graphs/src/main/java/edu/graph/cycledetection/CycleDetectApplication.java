aaa8554ab2166aa3d108de38cde4bf79508cc8f3
Prasad Gamini <>
2019-02-10 13:53:44 -0600

    CycleDetection

--- graphs/src/main/java/edu/graph/cycledetection/CycleDetectApplication.java
+++ graphs/src/main/java/edu/graph/cycledetection/CycleDetectApplication.java
@@ -1,4 +1,28 @@
 package edu.graph.cycledetection;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CycleDetectApplication {
+
+
+    public static void main(String[] args) {
+        Vertex v1 = new Vertex(1);
+        Vertex v2 = new Vertex(2);
+        Vertex v3 = new Vertex(3);
+        Vertex v4 = new Vertex(4);
+        Vertex v5 = new Vertex(5);
+        Vertex v6 = new Vertex(6);
+
+        v1.addNeighborVertices(v2, v3);
+        v2.addNeighborVertices(v3);
+//        v3.addNeighborVertices(v1);
+
+        v4.addNeighborVertices(v1, v5);
+        v5.addNeighborVertices(v6);
+        v6.addNeighborVertices(v4);
+
+        List<Vertex> graph = Arrays.asList(v1, v2, v3, v4, v5, v6);
+        new CycleDetection().detectCycle(graph);
+    }
 }
--- graphs/src/main/java/edu/graph/cycledetection/CycleDetection.java
+++ graphs/src/main/java/edu/graph/cycledetection/CycleDetection.java
@@ -1,4 +1,36 @@
 package edu.graph.cycledetection;
 
+import java.util.List;
+
 public class CycleDetection {
+
+    public void detectCycle(List<Vertex> graph) {
+
+        graph.stream().forEach(vertex -> {
+            if(!vertex.isVisited()) {
+                dfs(vertex);
+            }
+        });
+    }
+
+    private void dfs(Vertex vertex) {
+        System.out.println("DFS on vertex : " + vertex);
+        vertex.setBeingVisited(true);
+        vertex.setVisited(false);
+
+        for (Vertex neighbor: vertex.getNeighborsList()) {
+            if(neighbor.isBeingVisited()) {
+                System.out.println("We found a CYCLE backward edge : " + vertex + " , " + neighbor);
+                return;
+            }
+
+            if(!neighbor.isVisited()) {
+                neighbor.setVisited(true);
+                dfs(neighbor);
+            }
+        }
+
+        vertex.setBeingVisited(false);
+        vertex.setVisited(true);
+    }
 }
